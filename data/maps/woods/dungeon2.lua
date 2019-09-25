2a4b3a8343d1b72707165064b595a52304901505
Victor Lopez <lvictor_9@hotmail.com>
2019-09-24 21:05:00 -0700

    Add witch hut quest

--- data/maps/woods/dungeon2.lua
+++ data/maps/woods/dungeon2.lua
@@ -13,8 +13,82 @@ local game = map:get_game()
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+  local switch_path_a = map:get_entity("switch_path_a")
+  local block_path_a = map:get_entity("block_path_a")
+  function switch_path_a:on_activated()
+    block_path_a:set_enabled(false)
+  end
+  function switch_path_a:on_inactivated()
+    block_path_a:set_enabled()
+  end
+
+  local switch_path_b = map:get_entity("switch_path_b")
+  local block_path_b = map:get_entity("block_path_b")
+  function switch_path_b:on_activated()
+    block_path_b:set_enabled(false)
+  end
+  function switch_path_b:on_inactivated()
+    block_path_b:set_enabled()
+  end
+
+  local puzzle1 = map:get_puzzle("1")
+  local block_paths = map:get_entity("block_paths")
+
+  function puzzle1:on_solved()
+    block_paths:set_enabled()
+  end
+
+  local puzzle2 = map:get_puzzle("2")
+  local chest_path_b = map:get_entity("chest_path_b")
+
+  function puzzle2:on_solved()
+    chest_path_b:set_enabled()
+  end
+
+  function switch_main_door:on_activated()
+    gate_1:open()
+  end
+
+  local puzzle3 = map:get_puzzle("3")
+
+  function puzzle3:on_solved()
+    map.puzzle_3_solved = true
+  end
+
+  local puzzle4 = map:get_puzzle("4")
+
+  function puzzle4:on_solved()
+    map.puzzle_4_solved = true
+  end
+
+  function switch_straight_a:on_activated()
+    map.switch_straight_a_activated = true
+  end
+
+  function switch_straight_b:on_activated()
+    map.switch_straight_b_activated = true
+  end
+
+
+  local block_straight_right = map:get_entity("block_straight_right")
+
+  function switch_straight_c:on_activated()
+    if map.puzzle_3_solved == true and
+      map.puzzle_4_solved == true and
+      map.switch_straight_a_activated == true and
+      map.switch_straight_b_activated == true then
+        block_straight_right:set_enabled(false)
+    end
+  end
+
+  function switch_final:on_activated()
+    block_final:set_enabled(false)
+  end
+
+
+
+
+
 end
 
 -- Event called after the opening transition effect of the map,
--- data/maps/woods/witch_hut.lua
+++ data/maps/woods/witch_hut.lua
@@ -3,14 +3,26 @@ local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
-
+game:set_value("witch_clear", false)
 end
 
 function witch:on_interaction()
   if game:get_value("witch_clear") == nil or game:get_value("witch_clear") == false then
     game:start_dialog("woods.witch.1", function()
-      hero:start_treasure("cultured_meat")
-      game:set_value("witch_clear", true)
+    witch:prompt_item(function(item)
+          if not item then return end
+
+          local is_whiskey = item:get_name() == "whiskey"
+
+          if is_whiskey then
+            game:start_dialog("woods.witch.4", function()
+                hero:start_treasure("cultured_meat")
+                game:set_value("witch_clear", true)
+            end)
+          else
+            return
+          end
+        end)
     end)
   else
     game:start_dialog("woods.witch.4")
